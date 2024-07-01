771bbefe7892d1fde42de37fe05b5901e74b2604
Denis Kralj <denis@novu.co>
2024-07-01 18:58:26 +0200

    chore: align code with latest state
    PR comment adjustments

--- app/api/novu/route.ts
+++ app/api/novu/route.ts
@@ -1,4 +1,7 @@
-import { serve } from '@novu/framework/next';
-import { welcomeOnboardingEmail } from '../../novu/workflows';
+import { serve } from "@novu/framework/next";
+import { welcomeOnboardingEmail } from "../../novu/workflows";
 
-export const { GET, POST, OPTIONS } = serve({ workflows: [welcomeOnboardingEmail] });
+// the workflows collection can hold as many workflow definitions as you need
+export const { GET, POST, OPTIONS } = serve({
+  workflows: [welcomeOnboardingEmail],
+});
